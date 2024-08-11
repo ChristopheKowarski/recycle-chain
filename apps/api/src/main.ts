79e6eec3ded42cdef68859c6ff2ac8ddc8f052d1
ChristopheKowarski <christophe.kowarski@gmail.com>
2024-08-11 13:47:01 +0930

    Remove typo

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const a = 0
-  a = ''
   await app.listen(3000)
 }
 bootstrap()
